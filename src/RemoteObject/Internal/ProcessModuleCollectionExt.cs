e003cc0b35e12462e7f2d3da5bee28efd5b7d246
RemoteNet <remotenet@remotenet.com>
2021-10-17 19:57:51 +0300

    Add missing ProcessModuleCollectionExt file

---
+++ src/RemoteObject/Internal/ProcessModuleCollectionExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteObject.Internal
+{
+    public static class ProcessModuleCollectionExt
+    {
+        public static IEnumerable<ProcessModule> AsEnumerable(this ProcessModuleCollection collection)
+        {
+            for (var i = 0; i < collection.Count; i++)
+            {
+                yield return collection[i];
+            }
+        }
+    }
+}
