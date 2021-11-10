00e6e4dbbb579b348d930f49bea0455eb6de65ab
Xappy <noforu@gmail.com>
2021-11-10 19:18:51 +0200

    Retire Instructor, Rename solution

--- src/ScubaInstructor/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ScubaInstructor.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
