7dd50b1951f1b8dcfd4c892616c71d9f4d754041
Xappy <noforu@gmail.com>
2021-11-18 22:25:52 +0200

    Comments

--- src/RemoteNET/Internal/DynamicRemoteObject.cs
+++ src/RemoteNET/Internal/DynamicRemoteObject.cs
@@ -8,6 +8,13 @@ using System.Runtime.CompilerServices;
 
 namespace RemoteNET.Internal
 {
+    /// <summary>
+    /// A proxy of a remote object.
+    /// Usages of this class should be strictly as a `dynamic` variable.
+    /// Field/Property reads/writes are redirect to reading/writing to the fields of the remote object
+    /// Function calls are redirected to functions calls in the remote process on the remote object
+    /// 
+    /// </summary>
     [DebuggerDisplay("Dynamic Proxy of {" + nameof(__ro) + "}")]
     class DynamicRemoteObject : DynamicObject
     {
