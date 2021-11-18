f582aaaf2e94a4157ce2f09c38dd8ca3277b6ce7
Xappy <noforu@gmail.com>
2021-11-18 22:10:36 +0200

    Reworked returned address of pinned objs

--- src/ScubaDiver.API/Dumps/ObjectDump.cs
+++ src/ScubaDiver.API/Dumps/ObjectDump.cs
@@ -4,7 +4,14 @@ namespace ScubaDiver.API.Dumps
 {
     public class ObjectDump
     {
-        public ulong Address { get; set; }
+        /// <summary>
+        /// Address where the item was retrieved from
+        /// </summary>
+        public ulong RetrivalAddress { get; set; }
+        /// <summary>
+        /// Address when the item was freezed at when pinning. This address won't change until unpinning.
+        /// </summary>
+        public ulong PinnedAddress { get; set; }
         public string Type { get; set; }
         public string PrimitiveValue { get; set; }
         public List<MemberDump> Fields { get; set; }
