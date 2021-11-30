a7f4dbe105923140fca99fd19442bbfae72d327e
Xappy <noforu@gmail.com>
2021-11-30 19:01:50 +0200

    [Utils] Use other SHA256 class

--- src/RemoteNET/Internal/Extensions/HashUtils.cs
+++ src/RemoteNET/Internal/Extensions/HashUtils.cs
@@ -4,21 +4,21 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace RemoteNET.Internal
+namespace RemoteNET.Internal.Extensions
 {
     public static class HashUtils
     {
         public static string FileSHA256(string filePath)
         {
-            using (SHA256 SHA256 = SHA256Managed.Create())
+            using (SHA256 SHA256 = System.Security.Cryptography.SHA256.Create())
             using (FileStream fileStream = File.OpenRead(filePath))
             {
-                    return BitConverter.ToString(SHA256.ComputeHash(fileStream)).Replace("-", "").ToLowerInvariant();
+                return BitConverter.ToString(SHA256.ComputeHash(fileStream)).Replace("-", "").ToLowerInvariant();
             }
         }
         public static string BufferSHA256(byte[] buff)
         {
-            using (SHA256 SHA256 = SHA256Managed.Create())
+            using (SHA256 SHA256 = System.Security.Cryptography.SHA256.Create())
             {
                 return BitConverter.ToString(SHA256.ComputeHash(buff)).Replace("-", "").ToLowerInvariant();
             }
