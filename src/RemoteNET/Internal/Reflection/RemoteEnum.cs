70cef4ba3052229d84c53fca1dff2bd80292d006
RemoteNet <remotenet@remotenet.com>
2021-11-30 19:06:00 +0200

    Remote Enums

---
+++ src/RemoteNET/Internal/Reflection/RemoteEnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using ScubaDiver.API;
+
+namespace RemoteNET.Internal.Reflection
+{
+    public class RemoteEnum
+    {
+        private Type _remoteType;
+        public RemoteApp App => (_remoteType as RemoteType)?.App;
+
+        public RemoteEnum(Type remoteType)
+        {
+            _remoteType = remoteType;
+        }
+
+        public ObjectOrRemoteAddress GetValue(string valueName)
+        {
+            FieldInfo verboseField = _remoteType.GetField(valueName);
+            ObjectOrRemoteAddress logginVerboseOora = (ObjectOrRemoteAddress)verboseField.GetValue(null);
+            return logginVerboseOora;
+        }
+
+        public dynamic Dynamify()
+        {
+            return new DynamicRemoteEnum(this);
+        }
+    }
+}
