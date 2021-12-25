2a9d571e3b6acc66eaa2d3bae10a69a6f49dc200
Xappy <noforu@gmail.com>
2021-12-25 16:32:00 +0200

    [RemoteObject] Add FilePermissions helper
    So we can set DLLs permissions such that UWP apps can load them

---
+++ src/RemoteNET/Internal/Utils/FilePermissions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
+
+namespace RemoteNET.Internal.Utils
+{
+    public class FilePermissions
+    {
+        // Adds an ACL entry on the specified file for the specified account.
+        public static void AddFileSecurity(string fileName, string account,
+            FileSystemRights rights, AccessControlType controlType)
+        {
+
+            // Get a FileSecurity object that represents the
+            // current security settings.
+            FileInfo fInfo = new FileInfo(fileName);
+            FileSecurity fSecurity = FileSystemAclExtensions.GetAccessControl(fInfo);
+
+            // Add the FileSystemAccessRule to the security settings.
+            fSecurity.AddAccessRule(new FileSystemAccessRule(account,
+                rights, controlType));
+
+            // Set the new access settings.
+            FileSystemAclExtensions.SetAccessControl(fInfo, fSecurity);
+        }
+
+    }
+}
