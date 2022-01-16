542de864a7e0b52dfbf4692679957b283234a64d
RemoteNet <remotenet@remotenet.com>
2022-01-16 19:18:35 +0200

    [Diver] Restore dump-before-refresh logic
    An attempt to optimize remove the dumping of the object from the existing runtime before refreshing but the hashcode-fallback code (when the object moves) relied on having a previous ClrObj in hand so we know which MethodTable to search for.

--- src/ScubaDiver.API/Dumps/HeapDump.cs
+++ src/ScubaDiver.API/Dumps/HeapDump.cs
@@ -9,6 +9,7 @@ namespace ScubaDiver.API.Dumps
             public ulong Address { get; set; }
             public string Type { get; set; }
             public int HashCode { get; set; }
+            public ulong MethodTable { get; set; }
         }
 
         public List<HeapObject> Objects { get; set; }
