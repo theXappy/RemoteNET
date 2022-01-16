1b32c93b5133c92673a863c3a64985915d630cb2
RemoteNet <remotenet@remotenet.com>
2022-01-16 19:30:51 +0200

    [Logger/Adapter] Allow release debugging using env var
    Set REMOTE_NET_MAGIC_DEBUG to any value

--- src/RemoteNET/Logger.cs
+++ src/RemoteNET/Logger.cs
@@ -7,11 +7,20 @@ namespace RemoteNET
 {
     internal class Logger
     {
+        internal static Lazy<bool> _debugInRelease = new Lazy<bool>(() => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("REMOTE_NET_MAGIC_DEBUG")));
+
         internal static void Debug(string s)
         {
 #if DEBUG
-            Console.WriteLine(s);
+#else // RELEASE
+            // We want to allow debug writing in release only if the magic enviroment var is set.
+            // in debug, this `if` does not exist and logging to console always takes place.
+            if(_debugInRelease.Value)
 #endif
+            {
+                Console.WriteLine(s);
+            }
+
             if(Debugger.IsAttached)
             {
                 System.Diagnostics.Debug.WriteLine(s);
