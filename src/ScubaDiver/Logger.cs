fe36c83b688c745adee751d8e540d819f4c0e104
RemoteNet <remotenet@remotenet.com>
2022-01-22 23:38:44 +0200

    [Diver] Update logger with new conditions

--- src/ScubaDiver/Logger.cs
+++ src/ScubaDiver/Logger.cs
@@ -10,8 +10,15 @@ namespace ScubaDiver
         internal static void Debug(string s)
         {
 #if DEBUG
-            Console.WriteLine(s);
+#else // RELEASE
+            // We want to allow debug writing in release only if the magic enviroment var is set.
+            // in debug, this `if` does not exist and logging to console always takes place.
+            if(_debugInRelease.Value)
 #endif
+            {
+                Console.WriteLine(s);
+            }
+
             if (Debugger.IsAttached)
             {
                 System.Diagnostics.Debug.WriteLine(s);
