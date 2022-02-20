3d2e29425284534abc4a03b55e00a31b0fdfeb95
Xappy <noforu@gmail.com>
2022-02-20 21:40:00 +0200

    [RemoteEnum] Specify reflection api types used

--- src/RemoteNET/Internal/Reflection/RemoteEnum.cs
+++ src/RemoteNET/Internal/Reflection/RemoteEnum.cs
@@ -6,17 +6,22 @@ namespace RemoteNET.Internal.Reflection
 {
     public class RemoteEnum
     {
-        private readonly Type _remoteType;
+        private readonly RemoteType _remoteType;
         public RemoteApp App => (_remoteType as RemoteType)?.App;
 
-        public RemoteEnum(Type remoteType)
+        public RemoteEnum(RemoteType remoteType)
         {
             _remoteType = remoteType;
         }
 
-        public dynamic GetValue(string valueName)
+        public object GetValue(string valueName)
         {
-            FieldInfo verboseField = _remoteType.GetField(valueName);
+            // NOTE: This is breaking the "RemoteX"/"DynamicX" paradigm because we are effectivly returning a DRO here
+            // How did that happen?
+            // Well, Unlike RemoteObject which uses directly a remote token + TypeDump to read/write fields/props/methods
+            // RemoteEnum was created after RemoteType was defined and it felt much easier to utilize it.
+            // RemoteType itself, as part of the reflection API, returns DROs when invoked.
+            RemoteFieldInfo verboseField = _remoteType.GetField(valueName) as RemoteFieldInfo;
             return verboseField.GetValue(null);
         }
 
