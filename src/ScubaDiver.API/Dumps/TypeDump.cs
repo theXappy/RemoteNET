49360c0b9fbbad4c65329d140e299eb659367f0f
Xappy <noforu@gmail.com>
2022-03-05 21:55:34 +0200

    [TypeDump.TypeEvent] Fix wrong field usage

--- src/ScubaDiver.API/Dumps/TypeDump.cs
+++ src/ScubaDiver.API/Dumps/TypeDump.cs
@@ -118,7 +118,7 @@ namespace ScubaDiver.API.Dumps
             {
                 this.Name = ei.Name;
                 this.TypeFullName = ei.EventHandlerType.FullName;
-                this.TypeFullName = ei.EventHandlerType.Assembly.GetName().Name;
+                this.Assembly = ei.EventHandlerType.Assembly.GetName().Name;
             }
         }
 
