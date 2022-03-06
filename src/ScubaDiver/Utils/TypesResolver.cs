ca973f8a87b1a3dd8109b738584982e79bcd5257
RemoteNet <remotenet@remotenet.com>
2022-03-06 21:07:19 +0200

    [TypeResolve] Search in all app domains

--- src/ScubaDiver/Utils/TypesResolver.cs
+++ src/ScubaDiver/Utils/TypesResolver.cs
@@ -30,51 +30,67 @@ namespace ScubaDiver.Utils
             }
 
 
-            try
+            foreach (var appDom in _runtime.AppDomains)
             {
-                IList<ClrModule> assembliesToSearch = _runtime.AppDomains.First().Modules;
-                if (assembly != null)
-                    assembliesToSearch = assembliesToSearch.Where(mod => Path.GetFileNameWithoutExtension(mod.Name) == assembly).ToList();
-
-                foreach (ClrModule module in assembliesToSearch)
+                try
                 {
-                    ClrType clrTypeInfo = module.GetTypeByName(name);
-                    if (clrTypeInfo == null)
+                    IList<ClrModule> assembliesToSearch = appDom.Modules;
+                    if (assembly != null)
+                        assembliesToSearch = assembliesToSearch.Where(mod => Path.GetFileNameWithoutExtension(mod.Name) == assembly).ToList();
+
+                    foreach (ClrModule module in assembliesToSearch)
                     {
-                        var x = module.OldSchoolEnumerateTypeDefToMethodTableMap();
-                        var typeNames = (from tuple in x
-                                         let Token = tuple.Token
-                                         let ClrType = module.ResolveToken(Token) ?? null
-                                         where ClrType?.Name == name
-                                         select new { tuple.MethodTable, Token, ClrType }).ToList();
-                        if (typeNames.Any())
+                        ClrType clrTypeInfo = module.GetTypeByName(name);
+                        if (clrTypeInfo == null)
                         {
-                            clrTypeInfo = typeNames.First().ClrType;
+                            var x = module.OldSchoolEnumerateTypeDefToMethodTableMap();
+                            var typeNames = (from tuple in x
+                                             let Token = tuple.Token
+                                             let ClrType = module.ResolveToken(Token) ?? null
+                                             where ClrType?.Name == name
+                                             select new { tuple.MethodTable, Token, ClrType }).ToList();
+                            if (typeNames.Any())
+                            {
+                                clrTypeInfo = typeNames.First().ClrType;
+                            }
                         }
-                    }
 
-                    if (clrTypeInfo == null)
-                    {
-                        continue;
-                    }
+                        if (clrTypeInfo == null)
+                        {
+                            continue;
+                        }
 
-                    // Found it
-                    Type typeObj = clrTypeInfo.GetRealType();
-                    return typeObj;
+                        // Found it
+                        Logger.Debug($"[Diver][TypesResolver] Resolved type from ClrMD Dump in domain: {appDom.Name}");
+                        Type typeObj = clrTypeInfo.GetRealType();
+                        return typeObj;
+                    }
+                }
+                catch
+                {
+                    // Using ClrMD Failed but we have a fallback
                 }
             }
-            catch 
-            { 
-                // Using ClrMD Failed but we have a fallback
-            }
 
-            // Fallback - normal .NET reflection
-            foreach (Assembly assm in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                Type t = assm.GetType(name, throwOnError: false);
-                if (t != null)
+            // Fallback - normal .NET reflection in all assemblies of all domains
+            // BEWARE: Confitional compilation 🤢
+            // .NET Core deprecated "App Domains" as a concept
+            // Which means the AppDomain class still exists but the "_AppDomain" struct isn't...
+            // In a sense, .NET Core's only possible AppDomain instance is the one representing the current (and only) App Domain in the app.
+#if NETCOREAPP
+            foreach (AppDomain domain in CLRUtil.EnumAppDomains())
+#else
+            foreach (_AppDomain domain in CLRUtil.EnumAppDomains())
+#endif
+            { 
+                foreach (Assembly assm in domain.GetAssemblies())
                 {
-                    return t;
+                    Type t = assm.GetType(name, throwOnError: false);
+                    if (t != null)
+                    {
+                        Logger.Debug($"[Diver][TypesResolver] Resolved type with reflection in domain: {domain.FriendlyName}");
+                        return t;
+                    }
                 }
             }
 
