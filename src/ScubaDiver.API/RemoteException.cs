be34fa6852306fe81d7de0f9af1554e9e62d13aa
Xappy <noforu@gmail.com>
2022-03-18 10:13:12 +0200

    [DiverComm] Try to parse generic errors

---
+++ src/ScubaDiver.API/RemoteException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScubaDiver.API
+{
+    public class RemoteException : Exception
+    {
+        public string Message { get; private set; }
+        public RemoteException(string msg)
+        {
+            Message = msg;
+        }
+    }
+}
