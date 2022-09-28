15c8017b45a67e095596446fbdedc3cf0ec05b37
RemoteNet <remotenet@remotenet.com>
2022-09-28 23:07:54 +0300

    [API] Adopt better filter creation

--- src/ScubaDiver.API/Utils/Filter.cs
+++ src/ScubaDiver.API/Utils/Filter.cs
@@ -1,46 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ScubaDiver.API.Utils
 {
     public class Filter
     {
+        /// <summary>
+        /// Turn a "simple filter string" into a regex
+        /// </summary>
+        /// <param name="simpleFilter">A string that only allow '*' as a wild card meaning "0 or more characters"</param>
+        private static Regex SimpleFilterToRegex(string simpleFilter)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("^"); // Begining of string
+            foreach (char c in simpleFilter)
+            {
+                if (c == '*')
+                {
+                    sb.Append(".*");
+                }
+                else
+                {
+                    string asEscaped = Regex.Escape(c.ToString());
+                    sb.Append(asEscaped);
+                }
+            }
+            sb.Append("$"); // End of string
+            return new Regex(sb.ToString());
+        }
+
         public static Predicate<string> CreatePredicate(string filter)
         {
             Predicate<string> matchesFilter = (testee) => true;
             if (filter != null)
             {
+                Regex r = SimpleFilterToRegex(filter);
                 string noStartsFilter = filter.Trim('*');
-                // User specified a filter. Looking for wild cards
-                if (filter.StartsWith("*"))
-                {
-                    if (filter.EndsWith("*"))
-                    {
-                        // Filter of format "*phrase*", looking anywhere inside the type name
-                        matchesFilter = (typeName) => typeName.Contains(noStartsFilter);
-                    }
-                    else
-                    {
-                        // Filter of format "*phrase", looking for specific suffix
-                        matchesFilter = (typeName) => typeName.EndsWith(noStartsFilter);
-                    }
-                }
-                else
-                {
-                    if (filter.EndsWith("*"))
-                    {
-                        // Filter of format "phrase*", looking for specific prefix
-                        matchesFilter = (typeName) => typeName.StartsWith(noStartsFilter);
-                    }
-                    else
-                    {
-                        // Filter has no wildcards - looking for specific type
-                        matchesFilter = (typeName) => typeName == filter;
-                    }
-                }
+                // Filter has no wildcards - looking for specific type
+                matchesFilter = r.IsMatch;
             }
-
             return matchesFilter;
         }
     }
