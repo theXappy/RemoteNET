0fe2a054f7d6ecbea616050eaedf491cf3d616a0
Xappy <noforu@gmail.com>
2022-10-04 21:57:14 +0300

    [Diver] Add stack trace to errors

--- src/ScubaDiver.API/RemoteException.cs
+++ src/ScubaDiver.API/RemoteException.cs
@@ -4,10 +4,23 @@ namespace ScubaDiver.API
 {
     public class RemoteException : Exception
     {
-        public string Message { get; private set; }
-        public RemoteException(string msg)
+        public string RemoteMessage { get; private set; }
+        private string _remoteStackTrace;
+        public string RemoteStackTrace => _remoteStackTrace;
+        public override string StackTrace =>
+            $"{_remoteStackTrace}\n" +
+            $"--- End of remote exception stack trace ---\n" +
+            $"{base.StackTrace}";
+
+        public RemoteException(string msg, string remoteStackTrace)
         {
-            Message = msg;
+            RemoteMessage = msg;
+            _remoteStackTrace = remoteStackTrace;
+        }
+
+        public override string ToString()
+        {
+            return RemoteMessage;
         }
     }
 }
