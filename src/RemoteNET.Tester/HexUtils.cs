b259d46d7f710465f702fa148cd6ae48e0caa5a4
RemoteNet <remotenet@remotenet.com>
2023-02-03 19:59:33 +0200

    Improve hex dumper

--- src/RemoteNET.Tester/HexUtils.cs
+++ src/RemoteNET.Tester/HexUtils.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ScubaDiver.Tester
 {
     public static class HexUtils
     {
-        public static string ToHex(this IEnumerable<byte> bytes)
+        public static string ToHex(this object o)
+        {
+            IEnumerable remoteIenumerable = o as IEnumerable;
+            List<byte> combinedBytes = new List<byte>();
+            foreach (object o1 in remoteIenumerable)
+            {
+                byte b = (byte)o1;
+                combinedBytes.Add(b);
+            }
+
+            return ToHex(combinedBytes);
+        }
+
+        private static string ToHex(IEnumerable<byte> bytes)
         {
             return string.Join(separator: "", values: bytes.Select(b => b.ToString("X2")));
         }
