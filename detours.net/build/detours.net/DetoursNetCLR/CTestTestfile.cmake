2eaee6bebe5ac2194daabde12fed9d5cdd526a27
Xappy <noforu@gmail.com>
2023-06-03 12:56:04 +0300

    detours stuff

---
+++ detours.net/build/detours.net/DetoursNetCLR/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/git/RemoteNET/src/detours.net/detours.net/DetoursNetCLR
+# Build directory: C:/git/RemoteNET/detours.net/build/detours.net/DetoursNetCLR
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ detours.net/build/plugins/procmon/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/git/RemoteNET/src/detours.net/plugins/procmon
+# Build directory: C:/git/RemoteNET/detours.net/build/plugins/procmon
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
