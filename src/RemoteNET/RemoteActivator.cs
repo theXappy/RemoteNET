18bb67bee6dce9bc1a76463df6b09bb200ed1c91
RemoteNet <remotenet@remotenet.com>
2023-09-01 16:55:05 +0300

    [ManagedRemoteApp] Add Remote Marshal

--- src/RemoteNET/RemoteActivator.cs
+++ src/RemoteNET/RemoteActivator.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using RemoteNET.Internal.Reflection;
 using RemoteNET.Internal.Reflection.DotNet;
 using ScubaDiver.API;
 using ScubaDiver.API.Interactions;
