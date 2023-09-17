2b411b9e12fae079703c7ec1c5c81bdcdd8f609f
Xappy <noforu@gmail.com>
2023-09-17 17:03:15 +0300

    [ScubaDiver] CharStar

---
+++ src/ScubaDiver.API/CharStar.cs
@@ -0,0 +1,14 @@
+﻿namespace ScubaDiver.API
+{
+    public record CharStar
+    {
+        public nuint Address { get; set; }
+        public string Value { get; set; }
+
+        public CharStar(nuint address, string value)
+        {
+            Address = address;
+            Value = value;
+        }
+    }
+}
