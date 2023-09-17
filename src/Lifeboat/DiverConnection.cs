881e24785035eb1c2b7e4eb79624fa6a11b7fc33
RemoteNet <remotenet@remotenet.com>
2023-09-17 14:40:28 +0300

    [Lifeboat] Remove logs, add exception handlers

--- src/Lifeboat/DiverConnection.cs
+++ src/Lifeboat/DiverConnection.cs
@@ -26,67 +26,54 @@ namespace Lifeboat
 
         private void Reader()
         {
-            Console.WriteLine("[Reader] Started");
+            Console.WriteLine("[Diver Reader] Started");
             while (true)
             {
-                Console.WriteLine("[Reader] > Another loop iteration!");
-                Console.WriteLine("[Reader] Reading another response from Diver...");
-                HttpResponseSummary resp = SimpleHttpProtocolParser.ReadResponse(TcpClient);
-                Console.WriteLine($"[Reader] Reading response Finished! Is it null? {resp == null}");
+                HttpResponseSummary resp = null;
+                try
+                {
+                    resp = SimpleHttpProtocolParser.ReadResponse(TcpClient);
+                }
+                catch (IOException)
+                {
+                    break;
+                }
 
-                Console.WriteLine($"[Reader] Forwaring the response back to it's owner...");
+                if (resp == null)
+                    break;
+
                 _responses[resp.RequestId] = resp;
-                Console.WriteLine($"[Reader] Signaling owner's AutoResetEvent...");
                 _responseReceivedEvents[resp.RequestId].Set();
-                Console.WriteLine($"[Reader] > Finished another iteration!");
             }
+            Console.WriteLine("[Diver Reader] Exiting...");
         }
 
         private void Writer()
         {
-            Console.WriteLine("[Writer] Started. Reading from blocking collection...");
+            Console.WriteLine("[Diver Writer] Exiting...");
             foreach (HttpRequestSummary request in _requests.GetConsumingEnumerable())
             {
-                Console.WriteLine("[Writer] NEW request found in blocking collection!~!~!~");
-                Console.WriteLine("[Writer] writing to diver's TCP connection...");
                 SimpleHttpProtocolParser.Write(TcpClient, request);
-                Console.WriteLine("[Writer] Done writing to diver!");
             }
-
-            Console.WriteLine("[Writer] Enumeration Ended ?????????");
+            Console.WriteLine("[Diver Writer] Exiting...");
         }
 
         public async Task<HttpResponseSummary> SendAsync(HttpRequestSummary req)
         {
-            Console.WriteLine("[SendAsync] Entered");
-            Console.WriteLine($"[SendAsync] req == null ? {req == null}");
             string oldId = req.RequestId;
-            Console.WriteLine($"[SendAsync] Original Request ID: {oldId}");
-            Console.WriteLine("[SendAsync] Allocating ID");
             string newId = AllocateRequestId();
-            Console.WriteLine($"[SendAsync] Allocated! ID: {newId}");
-            Console.WriteLine($"[SendAsync] Setting ID to req.");
             req.RequestId = newId;
 
-            Console.WriteLine($"[SendAsync] Creating new AutoResetEvent");
             AutoResetEvent responseEvent = new AutoResetEvent(false);
-            Console.WriteLine($"[SendAsync] Adding new AutoResetEvent to dict");
             _responseReceivedEvents[newId] = responseEvent;
-            Console.WriteLine($"[SendAsync] AutoResetEvent created and added to dictionary");
 
-            Console.WriteLine("[SendAsync] Inserting request to _requests collection");
             _requests.Add(req);
-            Console.WriteLine("[SendAsync] Done adding to collection");
 
-            Console.WriteLine("[SendAsync] waiting on AutoResetEvent...");
             await responseEvent.WaitOneAsync();
-            Console.WriteLine("[SendAsync] AutoResetEvent SIGNALED!");
 
-            Console.WriteLine("[SendAsync] Reading response left by Reader...");
             _responseReceivedEvents.TryRemove(newId, out _);
             if (!_responses.TryRemove(newId, out HttpResponseSummary resp))
                 throw new Exception("Error: Reset event signaled but response was not found");
-            Console.WriteLine($"[SendAsync] Response found! Request ID before fixing: {resp.RequestId}");
 
             resp.RequestId = oldId;
             return resp;
@@ -99,21 +86,5 @@ namespace Lifeboat
             return id.ToString();
         }
 
-    }
-
-    public static class Extensions
-    {
-        public static Task WaitOneAsync(this WaitHandle waitHandle)
-        {
-            if (waitHandle == null)
-                throw new ArgumentNullException("waitHandle");
-
-            var tcs = new TaskCompletionSource<bool>();
-            var rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle,
-                delegate { tcs.TrySetResult(true); }, null, -1, true);
-            var t = tcs.Task;
-            t.ContinueWith((antecedent) => rwh.Unregister(null));
-            return t;
-        }
     }
 }
