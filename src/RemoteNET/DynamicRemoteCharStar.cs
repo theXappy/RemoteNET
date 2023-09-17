c96361c2de08d2f0776c4263d1e0bd8f48fe2576
Xappy <noforu@gmail.com>
2023-09-17 17:03:28 +0300

    [RemoteNet] RemoteChatStar

---
+++ src/RemoteNET/DynamicRemoteCharStar.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace RemoteNET;
+
+[DebuggerDisplay("Dynamic Proxy of char* { Value: " + nameof(_innerString) + "}")]
+public class DynamicRemoteCharStar : DynamicObject
+{
+    private string _innerString;
+
+    public DynamicRemoteCharStar(string initialValue)
+    {
+        _innerString = initialValue;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    {
+        result = _innerString.GetType().GetProperty(binder.Name)?.GetValue(_innerString);
+        if (result == null)
+        {
+            result = _innerString.GetType().GetField(binder.Name)?.GetValue(_innerString);
+        }
+
+        return result != null;
+    }
+
+    public override bool TrySetMember(SetMemberBinder binder, object value)
+    {
+        var propertyInfo = _innerString.GetType().GetProperty(binder.Name);
+        if (propertyInfo != null)
+        {
+            propertyInfo.SetValue(_innerString, value);
+            return true;
+        }
+
+        var fieldInfo = _innerString.GetType().GetField(binder.Name);
+        if (fieldInfo != null)
+        {
+            fieldInfo.SetValue(_innerString, value);
+            return true;
+        }
+
+        return false;
+    }
+
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+    {
+        var methodName = binder.Name;
+        var methodInfos = _innerString.GetType().GetMethods().Where(method => method.Name == methodName).ToList();
+
+        if (methodInfos.Count == 0)
+        {
+            result = null;
+            return false;
+        }
+
+        // Find the method with matching parameter types or compatible parameter types
+        var argumentTypes = args.Select(arg => arg.GetType()).ToArray();
+        var methodInfo = methodInfos.FirstOrDefault(m => ParametersMatch(m.GetParameters(), argumentTypes));
+
+        if (methodInfo == null)
+        {
+            result = null;
+            return false;
+        }
+
+        result = methodInfo.Invoke(_innerString, args);
+        return true;
+    }
+
+    private static bool ParametersMatch(ParameterInfo[] parameters, Type[] argumentTypes)
+    {
+        if (parameters.Length != argumentTypes.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            if (!parameters[i].ParameterType.IsAssignableFrom(argumentTypes[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static implicit operator string(DynamicRemoteCharStar obj) => obj._innerString;
+}
---
+++ src/RemoteNET/RemoteCharStar.cs
@@ -0,0 +1,31 @@
+using System;
+using ScubaDiver.API;
+
+namespace RemoteNET;
+
+public class RemoteCharStar : RemoteObject
+{
+    public override ulong RemoteToken { get; }
+    private string _val;
+
+    public RemoteCharStar(ulong remoteToken, string val)
+    {
+        RemoteToken = remoteToken;
+        _val = val;
+    }
+
+    public override dynamic Dynamify()
+    {
+        return new DynamicRemoteCharStar(_val);
+    }
+
+    public override ObjectOrRemoteAddress GetItem(ObjectOrRemoteAddress key)
+    {
+        throw new InvalidOperationException($"Can't call GetItem on a {typeof(RemoteCharStar)}");
+    }
+
+    public override Type GetRemoteType()
+    {
+        return typeof(CharStar);
+    }
+}
