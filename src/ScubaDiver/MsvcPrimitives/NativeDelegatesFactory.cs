bdceea4d2664d1ade2beb5054ab64c86bc09678c
RemoteNet <remotenet@remotenet.com>
2023-09-26 21:39:55 +0300

    [Native Delegates] extend support up to 15 args

--- src/ScubaDiver/MsvcPrimitives/NativeDelegatesFactory.cs
+++ src/ScubaDiver/MsvcPrimitives/NativeDelegatesFactory.cs
@@ -16,6 +16,11 @@ public static class NativeDelegatesFactory
     delegate void FuncVoidArgs8(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8);
     delegate void FuncVoidArgs9(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9);
     delegate void FuncVoidArgs10(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10);
+    delegate void FuncVoidArgs11(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11);
+    delegate void FuncVoidArgs12(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12);
+    delegate void FuncVoidArgs13(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13);
+    delegate void FuncVoidArgs14(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13, nuint _14);
+    delegate void FuncVoidArgs15(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13, nuint _14, nuint _15);
     delegate nuint FuncNUIntArgs0();
     delegate nuint FuncNUIntArgs1(nuint _1);
     delegate nuint FuncNUIntArgs2(nuint _1, nuint _2);
@@ -27,6 +32,11 @@ public static class NativeDelegatesFactory
     delegate nuint FuncNUIntArgs8(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8);
     delegate nuint FuncNUIntArgs9(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9);
     delegate nuint FuncNUIntArgs10(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10);
+    delegate nuint FuncNUIntArgs11(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11);
+    delegate nuint FuncNUIntArgs12(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12);
+    delegate nuint FuncNUIntArgs13(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13);
+    delegate nuint FuncNUIntArgs14(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13, nuint _14);
+    delegate nuint FuncNUIntArgs15(nuint _1, nuint _2, nuint _3, nuint _4, nuint _5, nuint _6, nuint _7, nuint _8, nuint _9, nuint _10, nuint _11, nuint _12, nuint _13, nuint _14, nuint _15);
 
     public static Type GetDelegateType(Type returnType, int numArguments)
     {
