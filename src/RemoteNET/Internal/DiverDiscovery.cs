1b06351128348e46c72bc49ac6f87e0343144a78
RemoteNet <remotenet@remotenet.com>
2024-09-27 11:56:47 +0300

    [DiverDiscovery] Reduce false positives port findings using GetExtendedTcpTable
    Previously, we'd just probe any port matching some PID.
    This sometimes led to waiting on non-cooperating ports forever in rnet-ps.
    
    This new strategy, relevant only for Windows, is using the WinAPI GetExtendedTcpTable which allows us to know not only which ports are opened for listening, but also the owner port.
    
    So we'red reducing the amount of problematic interactions to the cases where a process opens a listening TCP port on a port matching its own PID (and it's not a ScubaDiver)

--- src/RemoteNET/Internal/DiverDiscovery.cs
+++ src/RemoteNET/Internal/DiverDiscovery.cs
@@ -1,10 +1,11 @@
-﻿using RemoteNET.Internal.Extensions;
-using ScubaDiver.API;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using RemoteNET.Internal.Extensions;
+using ScubaDiver.API;
+using static Vanara.PInvoke.IpHlpApi;
 
 namespace RemoteNET.Internal
 {
@@ -35,15 +36,32 @@ namespace RemoteNET.Internal
             bool diverPortIsUse = false;
             try
             {
-                IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
-                IPEndPoint[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
-                List<int> usedPorts = tcpConnInfoArray.Select(conInfo => conInfo.Port).ToList();
+                if (OperatingSystem.IsWindows())
+                {
+                    // On Windows, can we confirm the suspected port is OPENED by the SPECIFIC TARGET PROCESS.
+                    using MIB_TCPTABLE_OWNER_MODULE openedListenPorts = GetExtendedTcpTable<MIB_TCPTABLE_OWNER_MODULE>(TCP_TABLE_CLASS.TCP_TABLE_OWNER_MODULE_LISTENER);
+                    ushort swappedPort = (ushort)((diverPort >> 8) | (diverPort << 8));
+                    var targetOpenedListenPorts = openedListenPorts.Where(row => row.dwLocalPort == diverPort || row.dwLocalPort == swappedPort).ToList();
+                    if (targetOpenedListenPorts.Any())
+                    {
+                        // I only ever expect a single process (or none) to hold the target port opened, but the API here is sh*t
+                        // and MIB_TCPROW_OWNER_MODULE is not comparable to `default`
+                        diverPortIsUse = targetOpenedListenPorts.Any(t => t.dwOwningPid == target.Id);
+                    }
+                }
+                else
+                {
+                    // Fallback for when we support non-windows hosts (yeah, right)
+                    // On non-Windows, can we only confirm the suspected port is OPENED by SOME process.
+                    IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+                    IPEndPoint[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+                    diverPortIsUse = tcpConnInfoArray.Any(conInfo => conInfo.Port == diverPort);
 
-                diverPortIsUse = usedPorts.Contains(diverPort);
+                }
             }
             catch
             {
-                // Had some issues, perhapse it's the diver holding that port.
+                // Had some issues, perhaps it's the diver holding that port.
             }
 
             if (diverPortIsUse)
