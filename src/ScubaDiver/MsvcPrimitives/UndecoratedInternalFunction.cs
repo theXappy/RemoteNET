cab23384a9c55b1c21ea545ac1116777a9255632
RemoteNet <remotenet@remotenet.com>
2025-01-01 20:23:57 +0200

    [MsvcDiver] UndecoratedInternalFunction: Fix no args

--- src/ScubaDiver/MsvcPrimitives/UndecoratedInternalFunction.cs
+++ src/ScubaDiver/MsvcPrimitives/UndecoratedInternalFunction.cs
@@ -1,4 +1,6 @@
 ﻿using ScubaDiver.Rtti;
+using System;
+using System.Linq;
 
 namespace ScubaDiver;
 
@@ -10,10 +12,21 @@ public class UndecoratedInternalFunction : UndecoratedFunction
     public override long Address => _address;
     public override ModuleInfo Module => _module;
 
-    public UndecoratedInternalFunction(string undecoratedName, string undecoratedFullName, string decoratedName, long address, int numArgs, ModuleInfo moduleInfo) 
+    private string[] _argTypes;
+    public override string[] ArgTypes => _argTypes;
+
+    public UndecoratedInternalFunction(
+        string undecoratedName, 
+        string undecoratedFullName, 
+        string decoratedName, 
+        long address, 
+        int numArgs, 
+        ModuleInfo moduleInfo) 
         : base(decoratedName, undecoratedName, undecoratedFullName, numArgs)
     {
         _address = address;
         _module = moduleInfo;
+
+        _argTypes = Enumerable.Repeat("long", numArgs).ToArray();
     }
 }
