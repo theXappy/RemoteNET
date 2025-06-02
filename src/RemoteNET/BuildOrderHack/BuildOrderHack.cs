f2fffd3518baf56f2f39710352a9e21ab6939566
RemoteNet <remotenet@remotenet.com>
2025-06-02 19:21:39 +0300

    GitHub Action n15: BuildOrderHack

---
+++ src/RemoteNET/BuildOrderHack/BuildOrderHack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteNET.BuildOrderHack
+{
+    internal class BuildOrderHack
+    {
+        static BuildOrderHack()
+        {
+            // SHOULD force building of Lifeboat before RemoteNET
+            typeof(Lifeboat.Program).ToString();
+        }
+    }
+}
