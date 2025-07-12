40544f81dffccecbc2733ee94fec245b08c4f3c4
RemoteNet <remotenet@remotenet.com>
2025-07-12 12:27:42 +0300

    <WIP> Source Generator 2

--- src/SourceGenerator/Generator.cs
+++ src/SourceGenerator/Generator.cs
@@ -1,102 +1,120 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using CliWrap.Buffered;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using RemoteNET;
-using RemoteNET.Access;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SourceGenerator
 {
     [Generator]
     public class Generator : ISourceGenerator
     {
+        void Log(string l)
+        {
+            File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", l);
+        }
+
         public void Initialize(GeneratorInitializationContext context)
         {
-            File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", "============ Generator Initialize started\n");
-
-            // Register a syntax receiver that will be created for each generation pass
-            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+            Log("============ Generator Initialize started\n");
         }
 
         public void Execute(GeneratorExecutionContext context)
         {
-            File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", "============ Listing ADditional Files\n");
-            foreach (var additionalFile in context.AdditionalFiles)
-            {
-                File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", $"Additional File: {additionalFile.Path}\n");
-            }
-            string TargetListPath = context.AdditionalFiles[0].Path;
-
-
-            var targetDlls = new List<string>();
+            // Cal Unsafe Executer with a try-catch and write the exception to the log
             try
             {
-                string[] lines = File.ReadAllLines(TargetListPath);
-                File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", "Read target list\n");
-
-                foreach (string line in lines)
-                {
-                    string trimmedLine = line.Trim();
-                    if (!string.IsNullOrEmpty(trimmedLine))
-                    {
-                        targetDlls.Add(trimmedLine);
-                        File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", $"Added target DLL: {trimmedLine}\n");
-                    }
-                }
+                UnsafeExecute(context);
             }
             catch (Exception e)
             {
-                File.AppendAllText(@"C:\Users\Shai\AppData\Local\Temp\logz\a.txt", $"Error reading target list: {e.Message}\n");
+                Log($"Error: {e.Message}\n");
+                Log($"StackTrace: {e.StackTrace}\n");
+                // If it's an aggregated exception, log all inner exceptions
+                if (e is AggregateException aggEx)
+                {
+                    foreach (var innerEx in aggEx.InnerExceptions)
+                    {
+                        Log($"Inner Exception: {innerEx.Message}\n");
+                        Log($"Inner StackTrace: {innerEx.StackTrace}\n");
+                    }
+                }
+                else
+                {
+                    Log($"Exception: {e.Message}\n");
+                    Log($"StackTrace: {e.StackTrace}\n");
+                }
+
+                return;
+            }
+        }
+
+        public void UnsafeExecute(GeneratorExecutionContext context)
+        {
+            Log("============ Listing ADditional Files\n");
+            foreach (var additionalFile in context.AdditionalFiles)
+            {
+                Log($"Additional File: {additionalFile.Path}\n");
+            }
+            string inspectedDllsFilePath = context.AdditionalFiles.FirstOrDefault(x => x.Path.EndsWith("InspectedDlls.txt"))?.Path;
+            string inspectedTypesFilePath = context.AdditionalFiles.FirstOrDefault(x => x.Path.EndsWith("InspectedTypes.txt"))?.Path;
+            if (inspectedDllsFilePath == null || inspectedTypesFilePath == null)
+            {
+                Log("InspectedDlls.txt or InspectedTypes.txt not found\n");
+                return;
+            }
+
+
+            // Create target with required DLLs
+            Process victim = Process.Start(@"C:\git\rnet-kit\RemoteNET\src\Tests\TestTarget\bin\Debug\net7.0-windows\TestTarget.exe");
+
+            List<string> targetDlls;
+            try
+            {
+                targetDlls = File.ReadAllLines(inspectedDllsFilePath).Select(x => x.Trim()).ToList();
+            }
+            catch (Exception e)
+            {
+                Log($"Error reading target list: {e.Message}\n");
                 Debug.WriteLine("Error reading target list: " + e.Message);
                 return;
             }
 
+            foreach (string dllPath in targetDlls)
+            {
+                Log("Injecting: {dllPath}");
+                var injectCommand =
+                    CliWrap.Cli.Wrap(@"C:\git\rnet-kit\rnet-inject\bin\Debug\net7.0-windows7.0\rnet-inject.exe")
+                    .WithWorkingDirectory(@"C:\git\rnet-kit\rnet-inject\bin\Debug\net7.0-windows7.0\")
+                    .WithArguments($"-u -t {victim.Id} -d {dllPath}")
+                    .WithValidation(CliWrap.CommandResultValidation.None)
+                    .ExecuteBufferedAsync();
 
-            // retreive the populated receiver 
-            if (!(context.SyntaxReceiver is SyntaxReceiver receiver))
-                return;
+                injectCommand.Task.Wait();
+                Log($"rnet-inject STDERR: {injectCommand.Task.Result.StandardError}\n");
+                Log($"rnet-inject STDOUT: {injectCommand.Task.Result.StandardOutput}\n");
+            }
+
+            var dumpCommand =
+                CliWrap.Cli.Wrap(@"C:\git\rnet-kit\rnet-class-dump\bin\Debug\net7.0-windows\rnet-class-dump.exe")
+                .WithArguments($"-u -t {victim.Id} -l {inspectedTypesFilePath}")
+                .WithValidation(CliWrap.CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+
+            dumpCommand.Task.Wait();
+            Log($"rnet-class-dump STDERR: {dumpCommand.Task.Result.StandardError}\n");
+            Log($"rnet-class-dump STDOUT: {dumpCommand.Task.Result.StandardOutput}\n");
 
             var compilation = context.Compilation;
 
-            // loop over the candidate fields, and keep the ones that are actually annotated
-            var symbols = new List<ITypeSymbol>();
-            foreach (var decl in receiver.ClassDeclarations)
-            {
-                var model = compilation.GetSemanticModel(decl.SyntaxTree);
-                if (model.GetDeclaredSymbol(decl, context.CancellationToken) is ITypeSymbol symbol)
-                {
-                    symbols.Add(symbol);
-                }
-            }
-
-
-
             context.AddSource($"all_types.cs", SourceText.From("p00p", Encoding.UTF8));
-        }
-    }
-
-    /// <summary>
-    /// Created on demand before each generation pass
-    /// </summary>
-    class SyntaxReceiver : ISyntaxReceiver
-    {
-        public List<ClassDeclarationSyntax> ClassDeclarations { get; } = new List<ClassDeclarationSyntax>();
-
-        /// <summary>
-        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
-        /// </summary>
-        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-        {
-            // any field with at least one attribute is a candidate for property generation
-            if (syntaxNode is ClassDeclarationSyntax decl)
-            {
-                ClassDeclarations.Add(decl);
-            }
+            return;
         }
     }
 }
