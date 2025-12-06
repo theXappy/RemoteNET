98232c3f7daf2ef7f45107be8c4caf6a5caf4b91
RemoteNet <remotenet@remotenet.com>
2025-12-06 13:18:53 +0200

    [Vessel] Refactor printed message (no cuneiform)

---
+++ src/Tests/RemoteNET.Vessel/VesselProgram.cs
@@ -0,0 +1,35 @@
+﻿namespace RemoteNET.Vessel
+{
+    public  class VesselProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(
+@"
+◰ ◲ ◱ ◳ ◰ ◲ ◱ ◳ ◰ ◲ ◱ ◳
+◳⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⣀⡀⠀⠀ ⠀⠀⠀⠀⠀◳
+◲⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⡄⢀⡀⠀⠀⠀⠀ ⠀◲
+◱⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⠀⠈⠙⢷⣄⠀⠀⠀⠀◱
+◰⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⢻⡆⠀⠀⠀◰
+◳⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⡄⠀⠀⠀⢸⡇⠀⠀⠀◳
+◲⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣷⡀⠀⠀⣿⠃⠀⠀⠀◲
+◱⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣷⡄⢸⠏⠀⠀⠀⠀◱
+◰⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀◰
+◳⠀⠀⠀⠀⢰⣿⣿⠉⣿⣿⣿⣿⠉⣿⣿⣧⠀⠀⠀⠀⠀◳
+◲⠀⠀⠀⠀⢸⣿⣿ ⣿⣿⣿⣿ ⣿⣿⣿⠀⠀⠀⠀⠀◲
+◱⠀⠀⠀⠀⠘⣿⣿⣿ ⣿⣿ ⣿⣿⣿⡟⠀⠀⠀⠀⠀◱
+◰⠀⠀⠀⠀⠀⢻⣿⣿⣿⣀⣀⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀◰
+◱⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠁ ⠀⠀⠀⠀ ◱
+◰ ◲ ◱ ◳ ◰ ◲ ◱ ◳ ◰ ◲ ◱ ◳");
+
+            TestClass t = new TestClass();
+            Console.WriteLine(t.ToString().Substring(0,0));
+
+            Thread.Sleep(TimeSpan.FromMinutes(100));
+
+            Console.WriteLine(t.ToString());
+            GC.KeepAlive(t);
+        }
+    }
+}
